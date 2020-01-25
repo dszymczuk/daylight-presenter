1d7d502af422b7527e49306c67580a0c8e747651
Damian <kontakt@dszymczuk.pl>
2020-01-25 17:17:38 +0100

    create input component

---
+++ src/components/input/index.js
@@ -0,0 +1,14 @@
+import { Form } from "react-bootstrap";
+import React from "react";
+
+const Input = ({ label, field, field: { name }, form: { errors } }) => (
+  <Form.Group controlId="name">
+    <Form.Label>{label}</Form.Label>
+    <Form.Control type={'text'} value={field.value} onChange={field.onChange} isInvalid={errors[name]}/>
+    {errors[name] && <Form.Control.Feedback type="invalid">
+      {errors[name]}
+    </Form.Control.Feedback>}
+  </Form.Group>
+);
+
+export default Input;
