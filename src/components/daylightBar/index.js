944aaa47d21eb3899831dfcc6806e3104b8dea87
Damian <kontakt@dszymczuk.pl>
2020-01-25 15:28:41 +0100

    update daylight bar to API

--- src/components/daylightBar/index.js
+++ src/components/daylightBar/index.js
@@ -4,18 +4,18 @@ import styles from './daylightBar.module.scss';
 import moment from "moment";
 
 
-const DaylightBar = ({ sunrise, sunset }) => {
+const DaylightBar = ({ daylight }) => {
+
+  const { sunrise, sunset } = daylight;
+
   const dayMinutes = 60 * 24;
 
-  const momentSunrise = moment(sunrise);
-  const momentSunset = moment(sunset);
-  const day = momentSunrise.date();
-  const month = momentSunrise.month();
-  const year = momentSunrise.year();
-
-  const initialDate = moment().date(day).month(month).year(year).hour(0).minute(0);
+  const momentSunrise = moment(sunrise, 'H:mm:ss A');
+  const momentSunset = moment(sunset, 'H:mm:ss A');
+  const initialDate = moment().hour(0).minute(0);
 
   const minutesBeforeSunrise = moment.duration(momentSunrise.diff(initialDate)).asMinutes();
+
   const minutesBeforeDaylight = dayMinutes - (dayMinutes - minutesBeforeSunrise);
 
   const daylightMinutes = moment.duration(momentSunset.diff(momentSunrise)).asMinutes();
