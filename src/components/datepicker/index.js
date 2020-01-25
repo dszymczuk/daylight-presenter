9f817c26caaf6ec3dd03b8142a881ddde03e0177
Damian <kontakt@dszymczuk.pl>
2020-01-25 16:13:16 +0100

    update styles for datepicker

--- src/components/datepicker/index.js
+++ src/components/datepicker/index.js
@@ -2,25 +2,28 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import moment from "moment";
+import styles from './datepicker.module.scss';
 
-
-export default ({ field: { name }, form: { setFieldValue, setFieldTouched } }) => (
-  <DatePicker
-    customInput={
-      <Form.Group controlId="date">
+export default ({ field: { name, value }, form: { setFieldValue, setFieldTouched } }) => {
+  return (
+    <div className={styles.datepickerWrapper}>
+      <Form.Group controlId={name}>
         <Form.Label>Date</Form.Label>
-        <Form.Control type={"text"}/>
+        <DatePicker
+          customInput={<Form.Control value={value}/>}
+          onChange={value => {
+            if (!!value) {
+              setFieldValue(name, value);
+              setFieldTouched(name, true);
+            }
+          }}
+          dateFormat={'dd.MM.yyyy'}
+          selected={value}
+          maxDate={new Date()}
+          showMonthDropdown
+          showYearDropdown
+        />
       </Form.Group>
-    }
-    onChange={value => {
-      if (!!value) {
-        const formattedDate = moment(value).format("DD.MM.YYYY");
-        setFieldValue(name, formattedDate);
-        setFieldTouched(name, true);
-      }
-    }}
-    showMonthDropdown
-    showYearDropdown
-  />
-)
+    </div>
+  )
+}
