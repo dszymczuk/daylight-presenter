3bd40723183c617975db8bf1c70d6226e86409c6
Damian <kontakt@dszymczuk.pl>
2020-01-25 16:31:27 +0100

    create DaylightDuringDay component

---
+++ src/components/daylightBar/daylightDuringDay.js
@@ -0,0 +1,35 @@
+import moment from "moment";
+import styles from "./daylightBar.module.scss";
+import { ProgressBar } from "react-bootstrap";
+import React from "react";
+
+const dayMinutes = 60 * 24;
+const initialDate = moment().hour(0).minute(0);
+
+const DaylightDuringDay = ({ sunrise, sunset }) => {
+  const momentSunrise = moment(sunrise, 'H:mm:ss A');
+  const momentSunset = moment(sunset, 'H:mm:ss A');
+
+  const minutesBeforeSunrise = moment.duration(momentSunrise.diff(initialDate)).asMinutes();
+
+  const minutesBeforeDaylight = dayMinutes - (dayMinutes - minutesBeforeSunrise);
+
+  const daylightMinutes = moment.duration(momentSunset.diff(momentSunrise)).asMinutes();
+  const minutesAfterDaylight = dayMinutes - minutesBeforeDaylight - daylightMinutes;
+
+  return (
+    <div className={styles.progressBarWrapper}>
+      <ProgressBar>
+        <ProgressBar variant="success" now={minutesBeforeDaylight} key={1} max={dayMinutes}/>
+        <ProgressBar variant="warning" now={daylightMinutes} key={2} max={dayMinutes}/>
+        <ProgressBar variant="success" now={minutesAfterDaylight} key={3} max={dayMinutes}/>
+      </ProgressBar>
+      <div className={styles.labels}>
+        <div className={styles.startHour}>00:00</div>
+        <div className={styles.endHour}>23:59</div>
+      </div>
+    </div>
+  )
+};
+
+export default DaylightDuringDay;
