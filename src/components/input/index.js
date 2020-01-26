70370119be03787a8f3372d902e396589cd8deee
Damian <kontakt@dszymczuk.pl>
2020-01-26 12:04:40 +0100

    update controlId in Input component

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -2,7 +2,7 @@ import { Form } from "react-bootstrap";
 import React from "react";
 
 const Input = ({ label, field, field: { name }, form: { errors } }) => (
-  <Form.Group controlId="name">
+  <Form.Group controlId={name}>
     <Form.Label>{label}</Form.Label>
     <Form.Control type={'text'} value={field.value} onChange={field.onChange} isInvalid={errors[name]}/>
     {errors[name] && <Form.Control.Feedback type="invalid">
